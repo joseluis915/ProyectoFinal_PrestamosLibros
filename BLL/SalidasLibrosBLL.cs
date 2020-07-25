150b4b96a2033d028739120252c655871acd66dc
Anderson-RM <anderson_rodriguez@ucne.edu.do>
2020-07-24 23:23:22 -0400

    Se creo y se programo la clase SalidasLibrosBLL y se programo la clase del registro rSalidasLibros

---
+++ BLL/SalidasLibrosBLL.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Using agregados
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+using ProyectoFinal_PrestamosLibros.DAL;
+using ProyectoFinal_PrestamosLibros.Entidades;
+
+namespace ProyectoFinal_PrestamosLibros.BLL
+{
+    public class SalidasLibrosBLL
+    {
+        //——————————————————————————————————————————————[ GUARDAR ]——————————————————————————————————————————————
+        public static bool Guardar(SalidasLibros salidasLibros)
+        {
+            if (!Existe(salidasLibros.SalidaLibroId))
+                return Insertar(salidasLibros);
+            else
+                return Modificar(salidasLibros);
+        }
+        //——————————————————————————————————————————————[ INSERTAR ]——————————————————————————————————————————————
+        private static bool Insertar(SalidasLibros salidasLibros)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.SalidasLibros.Add(salidasLibros);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+        //——————————————————————————————————————————————[ MODIFICAR ]——————————————————————————————————————————————
+        public static bool Modificar(SalidasLibros salidasLibros)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(salidasLibros).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+        //——————————————————————————————————————————————[ ELIMINAR ]——————————————————————————————————————————————
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var salidasLibros = contexto.SalidasLibros.Find(id);
+                if (salidasLibros != null)
+                {
+                    contexto.SalidasLibros.Remove(salidasLibros);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+        //——————————————————————————————————————————————[ BUSCAR ]——————————————————————————————————————————————
+        public static SalidasLibros Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            SalidasLibros salidasLibros;
+
+            try
+            {
+                salidasLibros = contexto.SalidasLibros.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return salidasLibros;
+        }
+        //——————————————————————————————————————————————[ GETLIST ]——————————————————————————————————————————————
+        public static List<SalidasLibros> GetList(Expression<Func<SalidasLibros, bool>> criterio)
+        {
+            List<SalidasLibros> lista = new List<SalidasLibros>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.SalidasLibros.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+        //——————————————————————————————————————————————[ EXISTE ]——————————————————————————————————————————————
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.SalidasLibros.Any(s => s.SalidaLibroId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+        //——————————————————————————————————————————————[ GET ]——————————————————————————————————————————————
+        public static List<SalidasLibros> GetPrestamos()
+        {
+            List<SalidasLibros> lista = new List<SalidasLibros>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.SalidasLibros.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
--- UI/Registros/rSalidasLibros.xaml.cs
+++ UI/Registros/rSalidasLibros.xaml.cs
@@ -9,34 +9,181 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+//Using agregados
+using ProyectoFinal_PrestamosLibros.BLL;
+using ProyectoFinal_PrestamosLibros.Entidades;
 
 namespace ProyectoFinal_PrestamosLibros.UI.Registros
 {
     public partial class rSalidasLibros : Window
     {
+        private SalidasLibros salidasLibros = new SalidasLibros();
         public rSalidasLibros()
         {
             InitializeComponent();
+            this.DataContext = salidasLibros;
         }
+        //——————————————————————————————————————————————————————————————[ Cargar ]———————————————————————————————————————————————————————————————
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = salidasLibros;
+        }
+        //——————————————————————————————————————————————————————————————[ Limpiar ]——————————————————————————————————————————————————————————————
+        private void Limpiar()
+        {
+            this.salidasLibros = new SalidasLibros();
+            this.DataContext = salidasLibros;
+        }
+        //——————————————————————————————————————————————————————————————[ Validar ]——————————————————————————————————————————————————————————————
+        private bool Validar()
+        {
+            bool Validado = true;
+            if (LibroIdTextBox.Text.Length == 0)
+            {
+                Validado = false;
+                MessageBox.Show("Transaccion Fallida", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
 
+            return Validado;
+        }
+        //——————————————————————————————————————————————————————————————[ Buscar ]———————————————————————————————————————————————————————————————
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
+            SalidasLibros encontrado = SalidasLibrosBLL.Buscar(Utilidades.ToInt(SalidaLibroIdTextbox.Text));
 
+            if (encontrado != null)
+            {
+                this.salidasLibros = encontrado;
+                Cargar();
+            }
+            else
+            {
+                this.salidasLibros = new SalidasLibros();
+                this.DataContext = this.salidasLibros;
+                MessageBox.Show($"Esta Entrada de libro no fue encontrada.\n\nAsegurese que existe o cree una nueva.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                Limpiar();
+                LibroIdTextBox.Focus();
+            }
         }
-
+        //——————————————————————————————————————————————————————————————[ Nuevo ]———————————————————————————————————————————————————————————————
         private void NuevoButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Limpiar();
         }
-
+        //——————————————————————————————————————————————————————————————[ Guardar ]———————————————————————————————————————————————————————————————
         private void GuardarButton_Click(object sender, RoutedEventArgs e)
         {
+            {
+                if (!Validar())
+                    return;
 
+                //———————————————————————————————————————————————————————[ VALIDAR SI ESTA VACIO ]———————————————————————————————————————————————————————
+                if (SalidaLibroIdTextbox.Text.Trim() == "")
+                {
+                    MessageBox.Show("El Campo (Libro Id) esta vacio.\n\nEscriba una Id de Libro.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                    SalidaLibroIdTextbox.Clear();
+                    SalidaLibroIdTextbox.Focus();
+                    return;
+                }
+
+                if (LibroIdTextBox.Text.Trim() == "")
+                {
+                    MessageBox.Show("El Campo (Libro Id) esta vacio.\n\nEscriba una Id de Libro.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                    LibroIdTextBox.Clear();
+                    LibroIdTextBox.Focus();
+                    return;
+                }
+                if (FechaDatePicker.Text.Trim() == "")
+                {
+                    MessageBox.Show($"El Campo (Fecha) esta vacio.\n\nEscriba una cantidad.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                    FechaDatePicker.Focus();
+                    return;
+                }
+
+                if (CantidadTextBox.Text.Trim() == "")
+                {
+                    MessageBox.Show($"El Campo (Cantidad) esta vacio.\n\nEscriba una cantidad.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                    CantidadTextBox.Clear();
+                    CantidadTextBox.Focus();
+                    return;
+                }
+
+                //———————————————————————————————————————————————————————[ VALIDAR SI ESTA VACIO - FIN ]———————————————————————————————————————————————————————
+                var paso = SalidasLibrosBLL.Guardar(salidasLibros);
+                if (paso)
+                {
+                    Limpiar();
+                    MessageBox.Show("Transaccion Exitosa", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                    MessageBox.Show("Transaccion Fallida", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
-
+        //——————————————————————————————————————————————————————————————[ Eliminar ]———————————————————————————————————————————————————————————————
         private void EliminarButton_Click(object sender, RoutedEventArgs e)
         {
+            {
+                if (SalidasLibrosBLL.Eliminar(Utilidades.ToInt(LibroIdTextBox.Text)))
+                {
+                    Limpiar();
+                    MessageBox.Show("Registro Eliminado", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                    MessageBox.Show("No se pudo eliminar el registro", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        //——————————————————————————————————————————[ SalidaLibroId - TextChanged ]——————————————————————————————————————————
+        private void EntradaLibroIdTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (SalidaLibroIdTextbox.Text.Trim() != "")
+                {
+                    int.Parse(SalidaLibroIdTextbox.Text);
+                }
+            }
+            catch
+            {
+                MessageBox.Show($"El valor digitado en el campo (EntradaLibro Id) no es un numero.\n\nPorfavor, digite un numero.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                SalidaLibroIdTextbox.Clear();
+                SalidaLibroIdTextbox.Focus();
+            }
+        }
+        //——————————————————————————————————————————[ LibroId - TextChanged ]——————————————————————————————————————————
+        private void LibroIdTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (LibroIdTextBox.Text.Trim() != "")
+                {
+                    int.Parse(LibroIdTextBox.Text);
+                }
+            }
+            catch
+            {
+                MessageBox.Show($"El valor digitado en el campo (Libro Id) no es un numero.\n\nPorfavor, digite un numero.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                LibroIdTextBox.Clear();
+                LibroIdTextBox.Focus();
+            }
+        }
 
+        //——————————————————————————————————————————[ Cantidad - TextChanged ]——————————————————————————————————————————
+        private void CantidadTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (CantidadTextBox.Text.Trim() != "")
+                {
+                    int.Parse(CantidadTextBox.Text);
+                }
+            }
+            catch
+            {
+                MessageBox.Show($"El valor digitado en el campo (Cantidad) no es un numero.\n\nPorfavor, digite un numero.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                CantidadTextBox.Clear();
+                CantidadTextBox.Focus();
+            }
         }
     }
 }
