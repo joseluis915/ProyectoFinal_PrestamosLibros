14c1c2ba54517f97c4c4cd5df29db98ba6475c7b
Anderson-RM <anderson_rodriguez@ucne.edu.do>
2020-07-25 00:52:52 -0400

    Se creo y programo la clase EditorialesBLL

---
+++ BLL/EditorialesBLL.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Using agregados
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+using ProyectoFinal_PrestamosLibros.DAL;
+using ProyectoFinal_PrestamosLibros.Entidades;
+
+namespace ProyectoFinal_PrestamosLibros.BLL
+{
+    public class EditorialesBLL
+    {
+        //——————————————————————————————————————————————[ GUARDAR ]——————————————————————————————————————————————
+        public static bool Guardar(Editoriales editoriales)
+        {
+            if (!Existe(editoriales.EditorialId))
+                return Insertar(editoriales);
+            else
+                return Modificar(editoriales);
+        }
+        //——————————————————————————————————————————————[ INSERTAR ]——————————————————————————————————————————————
+        private static bool Insertar(Editoriales editoriales)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Editoriales.Add(editoriales);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+        //——————————————————————————————————————————————[ MODIFICAR ]——————————————————————————————————————————————
+        public static bool Modificar(Editoriales editoriales)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(editoriales).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+        //——————————————————————————————————————————————[ ELIMINAR ]——————————————————————————————————————————————
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var editoriales = contexto.Editoriales.Find(id);
+                if (editoriales != null)
+                {
+                    contexto.Editoriales.Remove(editoriales);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+        //——————————————————————————————————————————————[ BUSCAR ]——————————————————————————————————————————————
+        public static Editoriales Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Editoriales editoriales;
+
+            try
+            {
+                editoriales = contexto.Editoriales.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return editoriales;
+        }
+        //——————————————————————————————————————————————[ GETLIST ]——————————————————————————————————————————————
+        public static List<Editoriales> GetList(Expression<Func<Editoriales, bool>> criterio)
+        {
+            List<Editoriales> lista = new List<Editoriales>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Editoriales.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+        //——————————————————————————————————————————————[ EXISTE ]——————————————————————————————————————————————
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Editoriales.Any(l => l.EditorialId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+        //——————————————————————————————————————————————[ GET ]——————————————————————————————————————————————
+        public static List<Editoriales> GetPrestamos()
+        {
+            List<Editoriales> lista = new List<Editoriales>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Editoriales.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
