8edc91648697512a05693460cb682d695ec01b71
Anderson-RM <anderson_rodriguez@ucne.edu.do>
2020-07-26 16:10:41 -0400

    Se diseño la consulta de EntradasLibros

---
+++ UI/Consultas/cEntradasLibros.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinal_PrestamosLibros.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cEntradasLibros.xaml
+    /// </summary>
+    public partial class cEntradasLibros : Window
+    {
+        public cEntradasLibros()
+        {
+            InitializeComponent();
+        }
+    }
+}
