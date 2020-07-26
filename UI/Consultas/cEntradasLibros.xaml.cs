dfa79a9db392f4a865da10ffb4b8cebd426e2c68
Anderson-RM <anderson_rodriguez@ucne.edu.do>
2020-07-26 16:13:50 -0400

    Se programo la clase de cEntradasLibros

--- UI/Consultas/cEntradasLibros.xaml.cs
+++ UI/Consultas/cEntradasLibros.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,17 +8,46 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+//Using agregados
+using System.Collections.Generic;
+using ProyectoFinal_PrestamosLibros.BLL;
+using ProyectoFinal_PrestamosLibros.Entidades;
 
 namespace ProyectoFinal_PrestamosLibros.UI.Consultas
 {
-    /// <summary>
-    /// Interaction logic for cEntradasLibros.xaml
-    /// </summary>
     public partial class cEntradasLibros : Window
     {
         public cEntradasLibros()
         {
             InitializeComponent();
+        }
+        private void ConsultarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<EntradasLibros>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = EntradasLibrosBLL.GetList(l => l.LibroId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Has dejado el Campo (Criterio) vacio.\n\nPor lo tanto, aparecerán todas las Entradas de Libros", "Informacion", MessageBoxButton.OK, MessageBoxImage.Information);
+                listado = EntradasLibrosBLL.GetList(c => true);
+            }
+
+            if (DesdeDatePicker.SelectedDate != null)
+                listado = EntradasLibrosBLL.GetList(c => c.Fecha.Date >= DesdeDatePicker.SelectedDate);
+
+            if (HastaDatePicker.SelectedDate != null)
+                listado = EntradasLibrosBLL.GetList(c => c.Fecha.Date <= HastaDatePicker.SelectedDate);
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
         }
     }
 }
