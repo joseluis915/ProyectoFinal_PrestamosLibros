754c6859c89262d4c9accf9e4fa2486fcd33e4c7
Anderson-RM <anderson_rodriguez@ucne.edu.do>
2020-07-26 15:37:35 -0400

    Se diseño la consulta cLibros y se hicieron cambios menores en las consultas Estudiantes y Usuarios

---
+++ UI/Consultas/cLibros.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinal_PrestamosLibros.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cLibros.xaml
+    /// </summary>
+    public partial class cLibros : Window
+    {
+        public cLibros()
+        {
+            InitializeComponent();
+        }
+    }
+}
