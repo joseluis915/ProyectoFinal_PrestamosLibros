2520c4af1205fa19889feff33f13adeb156cbf27
Anderson-RM <anderson_rodriguez@ucne.edu.do>
2020-07-26 15:48:58 -0400

    Se programo la clase de la consulta de libros

--- UI/Consultas/cLibros.xaml.cs
+++ UI/Consultas/cLibros.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,17 +8,46 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+//Using agregados
+using System.Collections.Generic;
+using ProyectoFinal_PrestamosLibros.BLL;
+using ProyectoFinal_PrestamosLibros.Entidades;
 
 namespace ProyectoFinal_PrestamosLibros.UI.Consultas
 {
-    /// <summary>
-    /// Interaction logic for cLibros.xaml
-    /// </summary>
     public partial class cLibros : Window
     {
         public cLibros()
         {
             InitializeComponent();
+        }
+        private void ConsultarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Libros>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = LibrosBLL.GetList(l => l.LibroId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Has dejado el Campo (Criterio) vacio.\n\nPor lo tanto, aparecerán todos los Libros", "Informacion", MessageBoxButton.OK, MessageBoxImage.Information);
+                listado = LibrosBLL.GetList(c => true);
+            }
+
+            if (DesdeDatePicker.SelectedDate != null)
+                listado = LibrosBLL.GetList(c => c.Fecha.Date >= DesdeDatePicker.SelectedDate);
+
+            if (HastaDatePicker.SelectedDate != null)
+                listado = LibrosBLL.GetList(c => c.Fecha.Date <= HastaDatePicker.SelectedDate);
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
         }
     }
 }
