1f4ab9e6d7fcdc1b9e570277aeaa68adf37fc659
Jose Luis Burgos Hernandez <jose_burgos3@ucne.edu.do>
2020-08-03 03:05:58 -0400

    - Se agregaron los test de las clases (SalidaLibrosBLL y UsuariosBLL)

---
+++ ProyectoFinal_PrestamosLibrosTest/BLL/SalidasLibrosBLLTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoFinal_PrestamosLibros.BLL;
+using ProyectoFinal_PrestamosLibros.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinal_PrestamosLibros.BLL.Tests
+{
+    [TestClass()]
+    public class SalidasLibrosBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            SalidasLibros salidasLibros = new SalidasLibros();
+
+            salidasLibros.SalidaLibroId = 1;
+            salidasLibros.UsuarioId = 1;
+            salidasLibros.LibroId = 1;
+            salidasLibros.Cantidad = 15;
+            salidasLibros.Fecha = DateTime.Now;
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.IsTrue(SalidasLibrosBLL.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.IsTrue(SalidasLibrosBLL.Buscar(1) != null);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.IsTrue(SalidasLibrosBLL.GetList(sl => sl.SalidaLibroId == 1) != null);
+        }
+
+        [TestMethod()]
+        public void ExisteTest()
+        {
+            Assert.IsTrue(SalidasLibrosBLL.Existe(1));
+        }
+
+        [TestMethod()]
+        public void GetSalidasLibrosTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ ProyectoFinal_PrestamosLibrosTest/BLL/UsuariosBLLTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoFinal_PrestamosLibros.BLL;
+using ProyectoFinal_PrestamosLibros.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoFinal_PrestamosLibros.BLL.Tests
+{
+    [TestClass()]
+    public class UsuariosBLLTests
+    {
+        [TestMethod()]
+        public void ExisteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Usuarios usuarios = new Usuarios();
+
+            usuarios.UsuarioId = 1;
+            usuarios.Nombres = "Juan";
+            usuarios.Apellidos = "Perez";
+            usuarios.FechaCreacion = DateTime.Now;
+            usuarios.NombreUsuario = "juanperez1";
+            usuarios.Contrasena = "juanperez1";
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.IsTrue(UsuariosBLL.Eliminar(1));
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.IsTrue(UsuariosBLL.Buscar(1) != null);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.IsTrue(UsuariosBLL.GetList(u => u.UsuarioId == 1) != null);
+        }
+
+        [TestMethod()]
+        public void AutenticarTest()
+        {
+            Usuarios usuarios = new Usuarios();
+            usuarios.NombreUsuario = "juanperez1";
+        }
+
+        [TestMethod()]
+        public void GetUsuariosTest()
+        {
+            Usuarios usuarios = new Usuarios();
+            usuarios.NombreUsuario = "juanperez1";
+            usuarios.Contrasena = "juanperez1";
+        }
+    }
+}
